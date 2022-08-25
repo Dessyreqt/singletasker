6478175de2c2bd5f80014c9ea4513ab8b934544d
David Carroll <carroll.david.s@gmail.com>
2022-08-25 16:31:19 -0500

    Add AppConfiguration object

---
+++ SingleTasker.Wpf/Configuration/AppConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace SingleTasker.Wpf.Configuration;
+
+using System;
+using System.IO;
+
+public class AppConfiguration
+{
+    public string TaskListPath { get; set; } = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "SingleTasker", "tasks.md");
+}
